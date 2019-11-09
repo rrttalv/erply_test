6db6eb12635ea1b80efbc679bec4beeaec447402
Rico Rein Talvar <ritalv@ttu.ee>
2019-11-09 09:15:01 +0200

    routing, font and home CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchPageComponent } from './components/search-page/search-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SearchPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
