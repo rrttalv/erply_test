50c9cf40ec8b0ef307543546bf022ce28afb1bc1
Rico Rein Talvar <ritalv@ttu.ee>
2019-11-09 10:11:25 +0200

    query form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +24,11 @@ import { SearchDisplayComponent } from './components/search-display/search-displ
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-search-form',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
-
+  @Output() search = new EventEmitter();
+  queryForm = new FormGroup({
+    companyVAT: new FormControl('', [Validators.pattern("(^[a-zA-Z]+[a-zA-Z])"), Validators.min(8), Validators.max(14)])
+  })
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get companyVAT(){
+    return this.queryForm.get('companyVAT')
+  }
+
+  errorList = [
+    {pattern: "Company VAT number has to start with country code"}, 
+    {min: "VAT number has to be at least 8 characters long"}, 
+    {max: "VAT number cannot be longer then 14 characters"}
+  ]
+
+  submitQuery(){
+    if(this.queryForm.valid){
+      this.search.emit(this.queryForm.value)
+    }
   }
 
 }
